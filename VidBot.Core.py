d2708afdbf92957e7de94de92df77446b6698a69
root <root@raspberrypi.(none)>
2014-02-27 08:32:02 +0000

    init test of stepper code works TODO : Add pygame keyboard monitoring 	Then, re-implement these
    Signed-off-by: root <root@raspberrypi.(none)>

--- VidBot.Core.py
+++ VidBot.Core.py
@@ -6,18 +6,26 @@ import time
 
 DEBUG = False
 RUNNING = True
-
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
+ObjStep = 22
+ObjDir = 27
+SlideStep = 17
+SlideDir = 4
 LEDR = 24
 LEDG = 18
 LEDB = 23
-
 LEDRoll = 0
+StepPause = 0.005 #0.001
 
 GPIO.setup(LEDR, GPIO.OUT)
 GPIO.setup(LEDG, GPIO.OUT)
 GPIO.setup(LEDB, GPIO.OUT)
+GPIO.setup(ObjStep, GPIO.OUT)
+GPIO.setup(ObjDir, GPIO.OUT)
+GPIO.setup(SlideStep, GPIO.OUT)
+GPIO.setup(SlideDir, GPIO.OUT)
 
 print ('Hello World!')
 
@@ -31,6 +39,38 @@ GPIO.output(LEDB, True)
 time.sleep(3)
 
 while RUNNING:
+    '''
+    ###############
+    #STEPPER MOTORS
+    ###############
+
+while x <= 5:
+    print(x)
+    x = x + 1
+    i = 0
+    j = 0
+
+    time.sleep(1)
+    GPIO.output(DirPin, GPIO.HIGH)
+
+    while i <= 250:
+        i = i + 1
+        GPIO.output(StepPin, GPIO.HIGH)
+        time.sleep(pause)
+        GPIO.output(StepPin, GPIO.LOW)
+        time.sleep(pause)
+        
+    time.sleep(.5)
+    GPIO.output(DirPin, GPIO.LOW)
+
+    while j <= 250:
+        j = j + 1
+        GPIO.output(StepPin, GPIO.HIGH)
+        time.sleep(pause)
+        GPIO.output(StepPin, GPIO.LOW)
+        time.sleep(pause)
+    '''
+
 
     '''
     #######
